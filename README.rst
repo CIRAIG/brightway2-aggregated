f7ad0685482e8287ad1bffbec6933f110493f8f9
Pascal Lesage <pascal.lesage@polymtl.ca>
2019-09-06 12:07:23 -0400

    License, readme, setup

---
+++ README.rst
@@ -0,0 +1,12 @@
+Brightway2-aggregated
+==========================
+
+.. image:: https://ci.appveyor.com/api/projects/status/uqixaochulbu6vjv?svg=true
+   :target: https://ci.appveyor.com/project/cmutel/brightway2-data
+   :alt: bw2data appveyor build status
+
+.. image:: https://coveralls.io/repos/bitbucket/cmutel/brightway2-data/badge.svg?branch=master
+    :target: https://coveralls.io/bitbucket/cmutel/brightway2-data?branch=default
+    :alt: Test coverage report
+
+This package provides tools for the creation and use of aggregated (i.e. cradle-to-gate) LCA data, both at the LCI and LCIA score level. It is an extension to the `Brightway2 LCA framework <https://brightwaylca.org>`_. `Online documentation <https://bw2agg.readthedocs.io/en/latest/>`_ is available, and the source code is hosted on `Github <https://github.com/pascallesage/brightway2-aggregated>`_.
--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 import setuptools
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-     name='bw2agg',
-     version='0.1',
-     author="Pascal Lesage",
-     author_email="pascal.lesage@polymtl.ca",
-     description="Extension to brightway2 package to work with aggregated data",
-     url="https://gitlab.com/pascal.lesage/brightway2-aggregated",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+    name='bw2agg',
+    version='0.1',
+    author="Pascal Lesage",
+    author_email="pascal.lesage@polymtl.ca",
+    description="Extension to brightway2 package to create and work with aggregated data",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/pascallesage/brightway2-aggregated",
+    install_requires=[
+        'bw2calc',
+        'bw2data',
+        'numpy',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+)
